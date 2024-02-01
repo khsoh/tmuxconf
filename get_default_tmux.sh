f30c4fa6834ad1c8183af9d949a95664555a3786
khsoh <2169449+khsoh@users.noreply.github.com>
2024-02-01 15:41:24 +0800

    Fixed tmux config to generate notes for key bindings

--- get_default_tmux.sh
+++ get_default_tmux.sh
@@ -1,11 +1,47 @@
 #!/bin/bash
-/opt/homebrew/bin/tmux -f /dev/null -L temp start-server \; show-options -g prefix| \
-  sed -r -e "s/(.*)/set-option -g \1/g" > ~/.tmux.reset.conf
 
-/opt/homebrew/bin/tmux -f /dev/null -L temp start-server \; list-keys | \
-  sed -r \
-  -e "s/bind-key(\s+)([\"#~\$])(\s+)/bind-key\1\'\2\'\3/g" \
-  -e "s/bind-key(\s+)([\'])(\s+)/bind-key\1\"\2\"\3/g" \
-  -e "s/bind-key(\s+)([;])(\s+)/bind-key\1\\\\\2\3/g" \
-  -e "s/command-prompt -I #([SW])/command-prompt -I \"#\1\"/g" \
-  >> ~/.tmux.reset.conf
+# Need to run in bash to obtain default key bindings
+
+tmux -f /dev/null -L temp start-server \; show-options -g prefix| \
+  sed -r -e "s/(.*)/set-option -g \1/g" > ~/.tmux1.reset.conf
+
+## == The following code is to get the notes for default tmux key bindings
+echo pkey=\(\) > ~/.tmux_keynotes
+echo pnote=\(\) >> ~/.tmux_keynotes
+
+tmux -f /dev/null -L temp start-server \; list-keys > ~/.tmux_default_keymap
+
+tmux -f /dev/null -L temp start-server \; list-keys -N | \
+  gsed -r \
+  -e "s/[^ a-zA-Z0-9\-]/\\\\&/g" \
+  -e "s/^\S+\s+(\S+)\s+(.*)$/pkey\+\=(\1)\npnote+=(\"\2\")/g" >> ~/.tmux_keynotes
+
+. ~/.tmux_keynotes
+## == end of getting the notes for default tmux key bindings into $pkey and $pnote
+
+IFS=$'\n'
+for line in $(cat ~/.tmux_default_keymap); do
+  kb=`echo $line | grep -E -o "\-T prefix\s+(\S+)"|grep -E -o "\S+$"`
+  if [[ -n $kb ]]; then
+    ekb=${kb//\\/}
+
+    ## There seems to be a bug in MacOS bash - the following array
+    #  search cannot be implemented into a function
+    idx=-1
+    for (( i = 0; i < ${#pkey[@]}; i++ )); do
+      if [[ "${pkey[$i]}" = "$ekb" ]]; then
+        idx=$i
+        break
+      fi
+    done
+
+    if [[ $idx < 0 ]]; then
+      echo $line >> ~/.tmux1.reset.conf
+    else
+      echo ${line/-T prefix/-N \"${pnote[$idx]}\" -T prefix} >> ~/.tmux1.reset.conf
+    fi
+  else
+    echo $line >> ~/.tmux1.reset.conf
+  fi
+done
+
