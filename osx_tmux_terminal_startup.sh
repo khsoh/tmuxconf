7c365605edf5db5affc57e8534ec3d78e829871c
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-06-26 11:48:27 +0800

    Disabled continuum-boot option in tmux Replaced tmux-continuum login startup script to work correctly in multi-monitor setup. Removed running of other tmux plugins because the tpm will execute those plugins.

---
+++ osx_tmux_terminal_startup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+start_terminal_and_run_tmux() {
+	osascript <<-EOF
+	tell application "Terminal"
+		if not (exists window 1) then reopen
+		activate
+		set winID to id of window 1
+		do script "tmux new-session -A" in window id winID
+	end tell
+	EOF
+}
+
+resize_window_to_half_screen() {
+
+	## Get the main display resolution
+	NIXRUNPATH=/run/current-system/sw/bin
+	JQ=$NIXRUNPATH/jq
+	SED=$NIXRUNPATH/sed
+	eval $(system_profiler SPDisplaysDataType -json|$JQ '.SPDisplaysDataType.[0].spdisplays_ndrvs.[] | select (.spdisplays_main == "spdisplays_yes") | ._spdisplays_resolution' | $SED -rn 's/"([0-9]+)\s*x\s*([0-9]+).*$/XX=\1;YY=\2/p')
+
+	## Compute the coordinates to position the terminal window in the main display.
+	## So, this works properly even in a multi-monitor setup.
+	TOPLEFTX=0
+	TOPLEFTY=25
+	BOTRIGHTX=$(( XX / 2 ))	# Setup in the left half of main display
+	BOTRIGHTY=$(( YY - 10 ))
+
+	osascript <<-EOF
+	tell application "Terminal"
+		set winID to id of window 1
+		set bounds of window id winID to {$TOPLEFTX, $TOPLEFTY, $BOTRIGHTX, $BOTRIGHTY}
+	end tell
+	EOF
+}
+
+main() {
+	start_terminal_and_run_tmux
+	resize_window_to_half_screen
+}
+main
